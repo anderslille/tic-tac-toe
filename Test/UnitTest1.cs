bae3d1a3515012fd05db53736397657abedfd50f
Anders Lilleheim <anders.lilleheim@bouvet.no>
2021-04-30 21:31:14 +0200

    Not_allow_consecutive_move_to_occupied_square

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -5,7 +5,8 @@ namespace Test
 {
     /*
      * 1.The game is played on a grid that's 3 squares by 3 squares.
-     * 2.The first player to play will be ‘X’ , while the next player will be‘O’. Players will alternate on putting their marks on the grid.
+     * 2.The first player to play will be ‘X’ , while the next player will be‘O’. 
+     *   Players will alternate on putting their marks on the grid.
      * 3.Players cannot play on an already played square. 
      * 4.The first player to get 3 marks in a row (either horizontally, vertically or diagonally) is the winner.
      * 5.When all 9 squaresare full, if no player has 3 marks in a row, the game ends in a tie
@@ -27,17 +28,16 @@ namespace Test
             _game = new TicTacToe();
         }
 
+        
         [Test]
         public void Return_X_As_First_Player()
-        {
-            
+        {   
             Assert.AreEqual("X", _game.GetCurrentPlayer());
         }
 
         [Test]
         public void Return_O_As_Second_Player()
         {
-
             _game.PlaceMarkerAt(1, 1);
 
             Assert.AreEqual("O", _game.GetCurrentPlayer());
@@ -46,7 +46,6 @@ namespace Test
         [Test]
         public void Return_X_As_Third_Player()
         {
-
             _game.PlaceMarkerAt(1, 1);
             _game.PlaceMarkerAt(1, 0);
 
@@ -56,7 +55,6 @@ namespace Test
         [Test]
         public void Return_O_As_Fourth_Player()
         {
-
             _game.PlaceMarkerAt(1, 1);
             _game.PlaceMarkerAt(1, 0);
             _game.PlaceMarkerAt(0, 1);
@@ -64,5 +62,13 @@ namespace Test
             Assert.AreEqual("O", _game.GetCurrentPlayer());
         }
 
+        [Test]
+        public void Not_allow_consecutive_move_to_occupied_square()
+        {
+            _game.PlaceMarkerAt(1, 1); 
+            _game.PlaceMarkerAt(1, 1); 
+
+            Assert.AreEqual("O", _game.GetCurrentPlayer());
+        }
     }
 }
--- src/TicTacToe.cs
+++ src/TicTacToe.cs
@@ -6,6 +6,8 @@ namespace src
     {
         private string _currentPlayer = "X";
 
+        private string _previouseMove = "";
+
         public TicTacToe()
         {
         }
@@ -15,8 +17,14 @@ namespace src
             return _currentPlayer;
         }
 
-        public void PlaceMarkerAt(int v1, int v2)
+        public void PlaceMarkerAt(int row, int col)
         {
+            if(_previouseMove == "" + row + col)
+            {
+                return;
+            }
+
+            _previouseMove = "" + row + col;
             if(_currentPlayer == "O")
             {
                 _currentPlayer = "X";
