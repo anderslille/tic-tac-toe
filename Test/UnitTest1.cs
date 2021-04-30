caf732bbfcd8374ecbd64a81c121176af3eab40f
Anders Lilleheim <anders.lilleheim@bouvet.no>
2021-04-30 19:53:31 +0200

    Place_X_On_First_Move()

--- Test/UnitTest1.cs
+++ Test/UnitTest1.cs
@@ -1,8 +1,24 @@
 using NUnit.Framework;
+using src;
 
 namespace Test
 {
-    public class Tests
+    /*
+     * 1.The game is played on a grid that's 3 squares by 3 squares.
+     * 2.The first player to play will be ‘X’ , while the next player will be‘O’. Players will alternate on putting their marks on the grid.
+     * 3.Players cannot play on an already played square. 
+     * 4.The first player to get 3 marks in a row (either horizontally, vertically or diagonally) is the winner.
+     * 5.When all 9 squaresare full, if no player has 3 marks in a row, the game ends in a tie
+     */
+
+    /*
+     * hvem sin tur er det
+     * 
+    * 
+    */
+
+
+    public class TicTacToeShould
     {
         [SetUp]
         public void Setup()
@@ -10,9 +26,16 @@ namespace Test
         }
 
         [Test]
-        public void Test1()
+        public void Place_X_On_First_Move()
         {
-            Assert.Pass();
+            // arrange
+            var game = new TicTacToe();
+            // act
+            var row = 1;
+            var col = 1;
+            game.PlaceMarkerAt(row, col);
+            //assert
+            Assert.AreEqual("X", game.GetMarkerAt(row, col));
         }
     }
 }
---
+++ src/TicTacToe.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace src
+{
+    public class TicTacToe
+    {
+        public TicTacToe()
+        {
+        }
+
+        public void PlaceMarkerAt(int row, int col)
+        {
+            
+        }
+
+        public string GetMarkerAt(int row, int col)
+        {
+            return "X";
+        }
+    }
+}
